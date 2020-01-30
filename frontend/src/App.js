b520ee30c551f07068b70e0538ce8d58191735a5
Filip Djuricic <fdjuricic98@gmail.com>
2020-01-30 21:18:33 +0100

    Add basic frontend routing, between Login & Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+import { Link, BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Music App</h1>
-    </div>
+    <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/register"}>Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
