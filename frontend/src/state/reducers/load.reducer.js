23fad18cf049634c46b6d26df911e6e44d2f4d42
Filip Djuricic <fdjuricic98@gmail.com>
2020-02-06 15:19:56 +0100

    Remove testing console.log

--- frontend/src/state/reducers/load.reducer.js
+++ frontend/src/state/reducers/load.reducer.js
@@ -6,7 +6,6 @@ const initialState = {
 
 export default function load(state = initialState, action) {
   if(action.type === TOGGLE_LOADING_SPINNER) {
-    console.log('FIRING!');
     return { ...state, isLoading: !state.isLoading };
   }
 
